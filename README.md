d8b05f15a2178bada2d2ed9257c15d8f1e9706ef
Indracit S <indrajithij802@gmail.com>
2023-05-06 17:11:13 +0530

    submitted in geeks for geeks

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 ## Java string problems
     1. Reverse words in a given string
-        Input: s = “i love programming very much” 
-        Output: s = “much very programming love i” 
+        test 1:
+            Input: s = “i love programming very much” 
+            Output: s = “much very programming love i” 
+        test 2:
+            in = "i.like.this.program.very.much"
+            out = "much.very.program.this.like.i"
--- src/Reverse_words.java
+++ src/Reverse_words.java
@@ -1,18 +1,39 @@
+import java.util.ArrayList;
+
 public class Reverse_words {
 
-    static String reverse(String words) {
-        String[] stringArr = words.split(" ");
+    static String reverseArr(ArrayList<String> s) {
         String result = "";
-        for (int i = stringArr.length - 1; i >= 0; i--) {
-
-            System.out.println(stringArr[i]);
-            result = result + stringArr[i] + " ";
+        for (int i = s.size() - 1; i >= 0; i--) {
+            result = result + s.get(i);
         }
-        return result.trim();
+        return result;
+    }
+
+    static String reverse(String words) {
+
+        char[] stringArr = words.toCharArray();
+        ArrayList<String> wordarr = new ArrayList<String>();
+        String result = "";
+        for (int i = 0; i <= stringArr.length - 1; i++) {
+
+            if (stringArr[i] == '.' || stringArr[i] == ' ') {
+                wordarr.add(result);
+                wordarr.add("" + stringArr[i]);
+                result = "";
+            } else {
+                result = result + stringArr[i];
+            }
+
+        }
+        wordarr.add(result);
+        return reverseArr(wordarr);
     }
 
     public static void main(String[] args) throws Exception {
-        String words = "i love programming very much";
-        System.out.println(reverse(words));
+        String test1 = "pqr.mno";
+        String test2 = "i.like.this.program.very.much";
+        System.out.println(reverse(test1));
+        System.out.println(reverse(test2));
     }
 }
