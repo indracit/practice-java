ff7fb9a6764ad5a91ebc8ca28a5bff0a3b5b96f6
Indracit S <indrajithij802@gmail.com>
2023-05-10 13:26:23 +0530

    paranthesis

---
+++ src/Paranthesis.java
@@ -0,0 +1,29 @@
+public class Paranthesis {
+
+    static boolean ispar(String x) {
+        int i = -1;
+        char[] a = new char[x.length()];
+
+        for (char c : x.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[') {
+
+                a[++i] = c;
+            } else if (i >= 0 && (a[i] == '(' && c == ')') || (a[i] == '{' && c == '}') || (a[i] == '[' && c == ']')) {
+
+                --i;
+            } else {
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "{(])}";
+
+        System.out.println(ispar(s));
+
+    }
+}
